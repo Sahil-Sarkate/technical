362dfde88e7b1378e0a331deeda27c1844e889d5
Sahil Sarkate <103311787+Sahil-Sarkate@users.noreply.github.com>
2023-06-03 10:11:51 +0000

    set homepages for github to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Sahil Sarkate",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "sarkatesahil@gmail.com",
+  gitHub: "sahilsarkate",
+  instagram: "sahilsarkate",
+  linkedIn: "sahilsarkate",
+  medium: " @sahilsarkate",
+  twitter: "sahilsarkate",
+  youTube: "sahilsarkate",
 };
 
 const primaryColor = "#4E567E";
